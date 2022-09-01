754456e23069a0833d51f974173e3863719a528c
Emanuel-34 <ema.boca.ferro34@gmail.com>
2022-09-01 18:25:49 -0300

    Segundo Commit

--- ejercicio10/src/ejercicio10/Ejercicio10.java
+++ ejercicio10/src/ejercicio10/Ejercicio10.java
@@ -15,7 +15,12 @@ public class Ejercicio10 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Realizar un programa que lea 4 números (comprendidos entre 1 y 20) e imprima el número
+//ingresado seguido de tantos asteriscos como indique su valor. Por ejemplo:
+//5 *****
+//3 ***
+//11 ***********
+//2 **
     }
     
 }
